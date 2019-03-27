e4f84b5e8193575b15fcfd4325acc1289a124006
9Anya9 <annasaliy909@gmail.com>
2019-03-28 00:35:35 +0200

    Initial commit

---
+++ Salii04/PersonInputView.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows.Controls;
+
+namespace Salii04
+{
+    /// <summary>
+    /// Interaction logic for PersonInputView.xaml
+    /// </summary>
+    internal partial class PersonInputView : UserControl
+    {
+        public PersonInputView(Action usersViewAction, Action<bool> loaderAction)
+        {
+            InitializeComponent();
+            DataContext = new PersonInputViewModel(usersViewAction, loaderAction);
+        }
+    }
+}
---
+++ Salii04/StationManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.IO;
+
+namespace Salii04
+{
+    internal static class StationManager
+    {
+        internal static readonly string WorkingDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Lab 04");
+        internal static Person CurrentPerson { get; set; }
+    }
+}
